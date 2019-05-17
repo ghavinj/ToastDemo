db1ed979e0538bcb47f796d14589b99b2aafbd1d
GhavinJ <ghavinj@icloud.com>
2019-05-17 19:03:32 -0400

    Added toast function, EditText, Toast

--- app/src/main/java/com/ghavinj/toastdemo/MainActivity.java
+++ app/src/main/java/com/ghavinj/toastdemo/MainActivity.java
@@ -2,8 +2,18 @@ package com.ghavinj.toastdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void toast(View view){
+
+        EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
+
+        Toast.makeText(this, "Hi "+nameEditText.getText().toString(), Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
